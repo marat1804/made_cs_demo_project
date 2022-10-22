cf646ddce7b03b6c74d77072cad5034c9f523111
Marat Khisamutdinov <maratbest6@gmail.com>
2022-10-22 17:06:54 +0300

    add tomorrow func

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ def now():
 	print(f"{datetime.datetime.now()}")
 
 
+def tomorrow():
+	print(f"{datetime.datetime.now() + datetime.timedelta(days=1)}")
+
+
 if __name__ == '__main__':
 	now()
+	tomorrow()
+
